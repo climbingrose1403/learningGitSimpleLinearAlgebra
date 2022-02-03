38b2720896c8b5afbd23d337a9ebadde33338a69
Nick Lebedintsev <61107398+climbingrose1403@users.noreply.github.com>
2022-02-03 18:24:45 +0300

    this class is now abstract in purpose to make accent to specific realizations, there are possibility that this can be changed

--- src/linearAlgebra/BasicObjects/Matrix.java
+++ src/linearAlgebra/BasicObjects/Matrix.java
@@ -4,10 +4,10 @@ import java.io.InputStream;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-/* this object contains Matrix and gives opportunities to configure and also do basic operations with single matrix.
-If you want to multiply two matrices you should use LinearOperations NOTA BENE PROBABLY SHOULD BE CHANGED
+/* this class contains basic operations that can be done with any kind of matrix
+
  */
-public class Matrix implements LinearObject{
+public abstract class Matrix implements LinearObject {
     final private int numberOfLines;
     final private int numberOfColumns;
     private Double[][] matrix;
@@ -24,16 +24,32 @@ public class Matrix implements LinearObject{
         setMatrix(System.in);
     }
 
+    public void setMatrix(Number[][] sourceArray) {
+        for (int i = 0; i < numberOfLines; i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                try {
+                    matrix[i][j] = (Double) sourceArray[i][j];
+                } catch (ClassCastException e) {
+                    System.out.println(" you");
+                }
+            }
+        }
+    }
+
     public void setMatrix(InputStream inputStreamSource) {
         Scanner sc = new Scanner(inputStreamSource);
         sc.useDelimiter(Pattern.compile("[ \n]"));
-        this.matrix = new Double[numberOfLines][numberOfColumns];
+        matrix = new Double[numberOfLines][numberOfColumns];
 
         for (int i = 0; i < numberOfLines; i++) {
             for (int j = 0; j < numberOfColumns; j++) {
                 matrix[i][j] = sc.nextDouble();
             }
         }
+    }
+
+    public void setMatrix(Double[][] sourceArray) {
+        matrix = sourceArray.clone();
     }
 
     public Double[][] getMatrix() {
@@ -53,6 +69,20 @@ public class Matrix implements LinearObject{
         }
     }
 
+    public static Double[][] getSlice(int numberOfExcludingLine, int numberOfExcludingColumn, Double[][] matrixElements) {
+        Double[][] slice = new Double[matrixElements.length - 1][matrixElements[0].length - 1];
+        for (int i = 0; i < matrixElements.length; i++) {
+            if (i != numberOfExcludingLine-1) {
+                for (int j = 0; j < matrixElements[0].length; j++) {
+                    if (j != numberOfExcludingColumn-1) {
+                        slice[i][j] = matrixElements[i][j];
+                    }
+                }
+            }
+        }
+        return slice;
+    }
+
     public void printMatrix() {
         for (int i = 0; i < numberOfLines; i++) {
             for (int j = 0; j < numberOfColumns; j++) {
@@ -62,5 +92,16 @@ public class Matrix implements LinearObject{
         }
     }
 
+    public String getStringMatrix() {
+        StringBuilder stringMatrix = new StringBuilder();
+        for (int i = 0; i < numberOfLines; i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                stringMatrix.append(matrix[i][j]);
+                stringMatrix.append(" ");
+            }
+            stringMatrix.append("\n");
+        }
+        return stringMatrix.toString();
+    }
 
 }
