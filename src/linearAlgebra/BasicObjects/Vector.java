5625701c073162b857951c7629907ee6f964e1b9
Nick Lebedintsev <61107398+climbingrose1403@users.noreply.github.com>
2022-02-03 18:28:08 +0300

    just a blank of the vector

--- src/linearAlgebra/BasicObjects/Vector.java
+++ src/linearAlgebra/BasicObjects/Vector.java
@@ -1,4 +1,7 @@
 package linearAlgebra.BasicObjects;
 
 public class Vector implements LinearObject{
+    /*this class characterize vector objects and their
+    attributes, can be operated with linear operations
+     */
 }
